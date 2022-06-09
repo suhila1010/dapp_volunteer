f44b4b2bd48a9c02a307eaaae72c55609377f7aa
suhila1010 <sohilaahmed678@gmail.com>
2022-06-10 01:07:17 +0200

    contract

--- my-app/contracts/volunteer.sol
+++ my-app/contracts/volunteer.sol
@@ -1,6 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-contract volunteer {
-    string  public name = "Volunteer app name";
+contract volunteer{
+    uint public numoffunders;
+    address public owner;
+
+    mapping(uint=>address) private funders;
+    mapping(address=>bool) private successfunders;
+
+    constructor(){
+        owner = msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender == owner , "only the owner can only withdraw.");
+        _;
+    }
+
+    function denote() public payable{
+        address funder = msg.sender;
+        if(!successfunders[funder]){
+            uint indx = numoffunders++;
+            funders[indx] = funder;
+            successfunders[funder] = true;
+        }
+    }
+
+    function getContractBalance()public view returns(uint){
+        return address(this).balance;
+    }
+
+    function withdraw(uint amount) public onlyOwner{
+        require(amount <= 20000000000000000 ,"the withdraw amount should be less than 0.2 ether.");
+        payable(msg.sender).transfer(amount);
+    }
+
+    
 }
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='bg-gray-100 text-red-500'>Start Point</h1>
+        <div className="text-blue-400 text-center font-bold">
+          <p>Denate To Our Project Contract To Help Poor People.</p>
+        </div>
+
+        <div className='sm:flex'>
+          <h3>My Wallet Address : </h3>
+          <h3> 0xf0Cce5CCc9AE5BC5a2b93375a385374c33Edd331</h3>
+        </div>
+        
+        <div className="bg-blue-500 text-violet-700 text-base font-semibold px-6 py-4 m-2 rounded-lg">
+          <h3>Contract Balance</h3>
+          <h2>2.5 ETH</h2>
+        </div>
+        
+        <div>
+        <button type="button" className="bg-blue-500 text-violet-700 text-base font-semibold px-6 py-2 rounded-lg">Donte Ether</button>
+        </div>
       </header>
     </div>
   );
